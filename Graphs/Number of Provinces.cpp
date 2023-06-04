6dd6025fdc7c54f7c84928efa386a9834e50d0a4
ano-ny-mous <nesar6174@gmail.com>
2023-06-04 11:44:13 +0530

    Add files via upload

---
+++ Graphs/Number of Provinces.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>> adj;
+
+    void dfs(int u,vector<bool> &visited)
+    {
+        visited[u]=true;
+        for(int &i:adj[u])
+        {
+            if(visited[i]==false)
+            {
+                dfs(i,visited);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int sz=isConnected.size();
+        vector<bool> visited(sz,false); 
+        for(int i=0;i<sz;i++)
+        {
+            for(int j=0;j<isConnected[0].size();j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+                    adj[i].push_back(j);
+                    cout<<i<<" "<<j<<endl;
+                }
+            }
+        }
+        int province=0;
+        for(int i=0;i<sz;i++)
+        {
+            if(visited[i]==false)
+            {
+                province++;
+                dfs(i,visited);
+            }
+        }
+        return province;
+    }
+};
